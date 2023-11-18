fd736702e309383d29e9a28b4c78e5ff59bd977c
wally <oaowally123@gmail.com>
2023-11-18 17:25:51 +0800

    11/18 第一頁 view 的 Control& Model

--- shoppingControl.php
+++ shoppingControl.php
@@ -3,21 +3,35 @@ require('todoModel.php');
 
 $act=$_REQUEST['act'];
 switch ($act) {
-case "listJob":
-  $jobs=getJobList(); //todoModel.php 的 getJobList()
-  echo json_encode($jobs);
-  return;  
-case "addJob":
-	$jobName=$_POST['name']; //$_GET, $_REQUEST
-	$jobUrgent=$_POST['urgent'];
-	$jobContent=$_POST['content'];
-	//verify
-	addJob($jobName,$jobUrgent,$jobContent);
+// case "listJob":
+//   $jobs=getJobList(); //todoModel.php 的 getJobList()
+//   echo json_encode($jobs);
+//   return;  
+// case "addJob":
+// 	$jobName=$_POST['name']; //$_GET, $_REQUEST
+// 	$jobUrgent=$_POST['urgent'];
+// 	$jobContent=$_POST['content'];
+// 	//verify
+// 	addJob($jobName,$jobUrgent,$jobContent);
+// 	return;
+// case "delJob":
+// 	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
+// 	//verify
+// 	delJob($id);
+// 	return;
+case "listProduct":
+	$products=listProduct();
+	echo json_encode($products);
 	return;
-case "delJob":
-	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
-	//verify
-	delJob($id);
+case "addCart":	
+	$id=(int)$_REQUEST['id'];
+	$qty=(int)$_REQUEST['qty'];
+	addCart($id,$qty);
+	return;
+case "getProductDetail":	
+	$id=(int)$_REQUEST['id'];
+	$product=getProductDetail($id);
+	echo json_encode($product);
 	return;
 default:
   
